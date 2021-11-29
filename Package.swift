c9a374fd49f05610999d33d298ed65debfe9b263
hvthhien <hvthhien@gmail.com>
2021-11-29 22:41:56 +0700

    update kukai-core

--- Package.swift
+++ Package.swift
@@ -15,7 +15,7 @@ let package = Package(
     dependencies: [
         .package(name: "Web3", url: "https://github.com/bitmark-inc/Web3.swift.git", .branch("master")),
         .package(url: "https://github.com/BlockchainCommons/URKit.git", .exact("2.4.0")),
-        .package(name: "KukaiCoreSwift", url: "https://github.com/hvthhien/kukai-core-swift.git", .branch("main"))
+        .package(name: "KukaiCoreSwift", url: "https://github.com/hvthhien/kukai-core-swift.git", .revision("549a9b9faee0c292c94b65db08e0cb464e18cbf6"))
         // Dependencies declare other packages that this package depends on.
         // .package(url: /* package url */, from: "1.0.0"),
     ],
