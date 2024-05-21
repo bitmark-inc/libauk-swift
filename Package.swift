738f871c0e0ea8a444930a82f6953590416d1aec
phuoc <phuoc@bitmark.com>
2024-05-21 16:57:48 +0700

    name package
    Signed-off-by: phuoc <phuoc@bitmark.com>

--- Package.swift
+++ Package.swift
@@ -14,11 +14,13 @@ let package = Package(
     ],
     dependencies: [
         .package(name: "Web3.swift", url: "https://github.com/bitmark-inc/Web3.swift.git", .branch("master")),
-        .package(url: "https://github.com/BlockchainCommons/URKit.git", .exact("14.0.2")),
+        .package(url: "https://github.com/BlockchainCommons/URKit.git", from: "14.0.2"),
         .package(url: "https://github.com/keefertaylor/Base58Swift.git", from: "2.1.0"),
         .package(name: "KukaiCoreSwift", url: "https://github.com/autonomy-system/kukai-core-swift.git", .branch("main")),
         .package(name: "TweetNacl", url: "https://github.com/bitmark-inc/tweetnacl-swiftwrap", branch: "master"),
-        .package(url: "https://github.com/BlockchainCommons/BCSwiftFoundation.git", .branch("master"))
+        .package(url: "https://github.com/BlockchainCommons/BCSwiftFoundation.git", .branch("master")),
+        .package(name: "secp256k1-zkp.swift", url: "https://github.com/BlockchainCommons/secp256k1-zkp.swift.git",from: "0.5.1"),
+        .package(name: "secp256k1.swift", url: "https://github.com/Boilertalk/secp256k1.swift.git", from: "0.1.7")
         // Dependencies declare other packages that this package depends on.
         // .package(url: /* package url */, from: "1.0.0"),
     ],
@@ -34,6 +36,8 @@ let package = Package(
                 .product(name: "Base58Swift", package: "Base58Swift"),
                 .product(name: "TweetNacl", package: "TweetNacl"),
                 .product(name: "BCFoundation", package: "BCSwiftFoundation"),
+                .product(name: "secp256k1_zkp", package: "secp256k1-zkp.swift"),
+                .product(name: "secp256k1_swift", package: "secp256k1.swift")
             ]),
         .testTarget(
             name: "LibAukTests",
