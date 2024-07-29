045fab6068b31393bae6526ac31b1efcfcf977a9
phuoc <phuoc@bitmark.com>
2024-07-29 14:25:09 +0700

    test pre gen limit
    Signed-off-by: phuoc <phuoc@bitmark.com>

--- Sources/LibAuk/Storage/SecureStorage.swift
+++ Sources/LibAuk/Storage/SecureStorage.swift
@@ -41,7 +41,7 @@ public protocol SecureStorageProtocol {
     func removeKeys() -> AnyPublisher<Void, Error>
     func setSeed(seed: Seed, isPrivate: Bool) -> AnyPublisher<Bool, Error>
     func removeSeed() -> AnyPublisher<Bool, Error>
-    func generateSeedPublicData(seed: Seed) -> AnyPublisher<SeedPublicData, Error>
+    func generateSeedPublicData(seed: Seed, preGenerateLimit: Int = 100) -> AnyPublisher<SeedPublicData, Error>
     func setData(_ data: Data, forKey: String, isSync: Bool, isPrivate: Bool) -> Bool
 }
 
